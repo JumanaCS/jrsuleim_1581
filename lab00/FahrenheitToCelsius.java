58fcef38bed193831f1f56f4ba89a92688062a04
Jumana Suleiman <148403239+JumanaCS@users.noreply.github.com>
2023-10-18 21:49:11 -0500

    Lab00

---
+++ lab00/FahrenheitToCelsius.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FahrenheitToCelsius{
+	public static void main(String[] args){
+		Scanner sc_object = new Scanner(System.in);
+		double fahrenheit = sc_object.nextDouble();
+		double celsius = (fahrenheit - 32) * (5.0/9.0);
+		System.out.println(celsius);
+	}
+}
